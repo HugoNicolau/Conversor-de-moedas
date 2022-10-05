e2475ceae3fb6641ca8f2751fb102ad5416a9a10
hugo <nicolau.hugogiles@gmail.com>
2022-10-05 17:55:10 -0300

    Apenas estrutura basica

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+
+export default function App(){
+    return(
+        <>
+        <p>Aqui vou começar a fazer meu conversor de moedas com Componentização, props, styled components, rotas e API (lembrar de instalar styled components e axios)</p>
+    <p>Vou usar a API https://economia.awesomeapi.com.br/json/last/USD-BRL da awesome</p>
+    </>
+        )
+}
